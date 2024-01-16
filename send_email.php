ec2a960ee83bbcf7cde202d8fade3833e3c2dab7
Aleem Akhtar <46089509+aleem57@users.noreply.github.com>
2024-01-16 16:57:30 +0530

    Add files via upload

--- send_email.php
+++ send_email.php
@@ -1,10 +1,14 @@
 <?php
 $errors = [];
 
+$success = false;
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get POST data
     $name = isset($_POST['name']) ? strip_tags(trim($_POST['name'])) : '';
     $email = isset($_POST['email']) ? trim($_POST['email']) : '';
+	$phone_number = isset($_POST['phone_number']) ? strip_tags(trim($_POST['phone_number'])) : '';
+	$subject = isset($_POST['subject']) ? strip_tags(trim($_POST['subject'])) : '';
     $message = isset($_POST['message']) ? strip_tags(trim($_POST['message'])) : '';
 
     // Validate form fields
@@ -30,11 +34,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Additional headers
         $headers = "From: $name <$email> <$phone_number> <$subject> <$message>";
 
+		$body = "<p>Name:".$name."</p>";
+		$body .= "<p>Email:".$email."</p>";
+		$body .= "<p>Phone Number:".$phone_number."</p>";
+		$body .= "<p>Subject:".$subject."</p>";
+		$body .= "<p>Message:".$message."</p>";
+		
         // Send email
-        if (mail($recipient, $message, $headers)) {
-            echo "Email sent successfully!";
-        } else {
-            echo "Failed to send email. Please try again later.";
+        if (mail($recipient,"COntact us form", $body, $headers)) {
+			$success = true;
+            //echo "Email sent successfully!";
         }
     } else {
         // Display errors
@@ -43,9 +52,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             echo "- $error<br>";
         }
     }
-} else {
-    // Not a POST request, display a 403 forbidden error
-    header("HTTP/1.1 403 Forbidden");
-    echo "You are not allowed to access this page.";
 }
 ?>
